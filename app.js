5e4894815254047e702c727e8b4809fa53d78e57
unknown <sampathkumar.ksk528@gmail.com>
2023-02-21 13:33:40 +0530

    allow all methods

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ console.log("Application Environment : " + environmant);
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "*");
     next();
 });
